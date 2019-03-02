d66e82b045ce25efe8dc269ea2922069ca3f3fe3
efen <efenstakes101@gmail.com>
2019-03-02 13:35:27 +0300

    initial app setup with slim and folder structure setup setup

---
+++ db.sql
@@ -0,0 +1,49 @@
+
+-- 
+drop database if exists sority;
+
+create database if not exists sority;
+
+use sority;
+
+
+create table users (
+    id int auto_increment,
+    name varchar(34) not null,
+    password varchar(200) not null,
+    phone varchar(15),
+    user_type enum('REGULAR', 'ADMIN'),
+    primary key(id)
+);
+
+
+create table security_spots (
+    id int auto_increment,
+    name varchar(34) not null,
+    city varchar(20),
+    lat varchar(20),
+    lng varchar(20),
+    spot_type enum ('HOSPITAL', 'POLICE_STATION'),
+    primary key(id)
+);
+
+
+create table alerts(
+    id int auto_increment,
+    from_user int not null,
+    from_lat varchar(200),
+    from_lng varchar(200),
+    alert_text longtext,
+    foreign key(from_user) references users(id),
+    primary key(id)
+);
+
+
+
+create table alert_buddies (
+  id int,
+  me_id int,
+  buddy_id varchar(15),
+  foreign key(me_id) references users(id),
+  primary key(id)
+);
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php 
+   /*
+     @author efen
+     @first-writing 07/11/2017 d/m/y
+     @doc the application entry page
+   */
+
+    // enable sessions
+    session_start();
+    
+    // allow cors requests
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'); 
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+    header("Access-Control-Allow-Headers: content-type, authorization");
+    header("Access-Control-Allow-Credentials: true");
+
+    // include downloaded libraries
+    require_once("./vendor/autoload.php");
+
+    // include slim classes from their namespaces
+    use Slim\Http\Request;
+    use Slim\Http\Response;
+    use Slim\Http\UploadedFile;
+
+    // include app classes 
+    // require_once("sority/models/User.php");
+    // require_once("sority/models/SecuritySpot.php");
+    // require_once("sority/models/Utility.php");
+
+    // setup slim
+    $app = new Slim\App([
+           'settings' => [ 'displayErrorDetails' => true, 'debug' => true, ]
+    ]);
+
+    $container = $app->getContainer();
+
+    // add view to the container
+    // using slim template lang
+    $container['view'] = function($cont){
+        $template_dir = __DIR__ . "/sority/views/";
+        $cache = false;
+        
+        return new Slim\Views\Twig($template_dir, compact('cache')); 
+    };
+
+
+    // start handling routes
+    // include routing files
+    // include_once("./sority/routes/api/user_routes.php");
+
+
+
+    
+    // check if a session exists 
+    function has_session(){
+       return (!empty($_SESSION['app_session']) && !empty($_SESSION['app_session']['user_id']));
+    }// end of has_session() 
+    
+    // return the session
+    function get_session(){
+       return $_SESSION;
+    }// end of get_session()
+    
+    /* 
+      check if a session is valid
+      validity is determined by existence of a session and.. 
+      a host whose data coincides with the session
+    **/
+    function is_session_valid(){
+      $return = false;
+      
+      if(has_session()){
+         $user_id = get_session()['app_session']['user_id'];
+         $user_type = get_session()['app_session']['user_type'];
+         $return = true; // return exists status for the entity 
+      }
+      return $return;
+    }
+    
+    // delete a session if it exists
+    function delete_session(){
+        unset($_SESSION['app_session']);
+    }// end of delete_session()
+    
+
+    // moves an upload file to the passed directory
+    function moveUploadedFile($directory, UploadedFile $uploadedFile){
+      $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+      $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+      $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+      $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+      return $filename;
+    }// end of get_and_return_data() 
+
+
+    // run the app
+    $app->run();
+
+?>
